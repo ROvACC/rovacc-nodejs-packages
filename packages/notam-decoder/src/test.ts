4b60b239f130eb277f68dbc970468a67a578762a
George Enciu <george.enciu@gmail.com>
2023-10-15 13:25:30 +0200

    fix: publish notam decoder
    ref : #28

--- packages/notam-decoder/src/test.ts
+++
@@ -1,7 +0,0 @@
-import { decode } from './parse';
-
-const notam = `(A4966/23 NOTAMN
-Q) LRBB/QMRLC/IV/NBO/A /000/999/4434N02605E005
-A) LROP B) 2310170600 C) 2310171000
-E) RWY 08R/26L CLSD)`;
-decode(notam);
